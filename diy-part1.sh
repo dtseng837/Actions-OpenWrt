38aa22ede76aa35f01588a706162fd44555ed794
dtseng837 <101380062+dtseng837@users.noreply.github.com>
2024-06-03 07:57:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,20 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci' >> feeds.conf.default
+
+
+
+# echo '添加jerrykuku的argon-mod主题'
+# rm -rf package/lean/luci-theme-argon  
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+# echo '=========Add argon-mod OK!========='
+
+# 添加gn 临时方案
+# cd package
+# svn co https://github.com/kenzok8/small-package/trunk/gn
